a97adea6b4440ae1ba392f07b343680ddc841cb5
Kathleen <burnsk15@yahoo.com>
2022-12-05 11:25:21 -0500

    Add function to determine which operator called and call appropriate math function.

--- myScript.js
+++ myScript.js
@@ -11,6 +11,16 @@ let multiple = (a, b) => a * b;
 let divide = (a, b) => a / b;
 
 // function to take two values and an operator and call one of the above functions
-
+let operate = function(a, b, operator) {
+    if (operator === "+") {
+        return add(a, b);
+    } else if (operator === "-") {
+        return subtract(a, b);
+    } else if (operator === "*") {
+        return multiple(a, b);
+    } else if (operator === "/") {
+        return divide(a, b);
+    }
+}
 
 // function to display buttons pushed and results
