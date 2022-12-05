06fce1eb35a7932904f7ab48ffae6ec4fec2d590
Kathleen <burnsk15@yahoo.com>
2022-12-05 11:03:49 -0500

    Add functions to add, subtract, multiply, and divide two operands.

---
+++ myScript.js
@@ -0,0 +1,16 @@
+// function to add operands
+let add = (a, b) => a + b;
+
+// funtion to subtract operands
+let subtract = (a, b) => a - b;
+
+// function to multiple operands
+let multiple = (a, b) => a * b;
+
+// function to divide operands
+let divide = (a, b) => a / b;
+
+// function to take two values and an operator and call one of the above functions
+
+
+// function to display buttons pushed and results
